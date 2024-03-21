6a933b773b501c8ab1d061964d40fc165e25c78a
Prada <jacoboprza2015@gmail.com>
2024-03-20 21:37:13 -0500

    Lvl.2 con enemigos y monedas, dialogo arreglado, gameOver puesto

--- Assets/aparecerVentana.cs
+++ Assets/aparecerVentana.cs
@@ -10,6 +10,10 @@ public class aparecerVentana : MonoBehaviour, MMEventListener<PickableItemEvent>
     [SerializeField] private GameObject ventana;
     //public Textos textos;
     [SerializeField] private string dialogo;
+    private int cont=0;
+
+
+
   // Interfaz con el evento de pickable item event que tiene las monedas de corgi
 
 
@@ -24,11 +28,14 @@ public class aparecerVentana : MonoBehaviour, MMEventListener<PickableItemEvent>
 
     public virtual void OnMMEvent(PickableItemEvent e)
     {
+        cont+=1;
         Debug.Log(e.PickedItem.name + "" + e.PickedItem.tag);
         if (e.PickedItem.tag == "Dialogo")
         {
+            if (cont >=2){
             ventana.SetActive(true);
             FindObjectOfType<ControlDialogos>().ActivarCartel(dialogo);
+            }
         }
     }
 }
